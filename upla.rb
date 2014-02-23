5313c0058a381226595e200ca9f27cf714c682b8
Aurélien Léger <enyriel@gmail.com>
2014-02-23 17:11:41 +0100

    Uploaded file can't be empty and use nicer flash messages.

--- upla.rb
+++ upla.rb
@@ -6,18 +6,22 @@ require 'sinatra/flash'
 enable :sessions
 
 get '/' do
-	slim :home
+  slim :home
 end
 
 # Handle POST-request (Receive and save the uploaded file)
 post '/upload' do
-	File.open('public/uploads/' + params['file_to_upload'][:filename], "w") do |f|
-		if f.write(params['file_to_upload'][:tempfile].read)
-			flash[:info] = "Fichier envoy&eacute; avec succ&egrave;s."
-		else
-			flash[:warning] = "Une erreur est survenue."
-		end
-	end
+  unless params['file_to_upload'].nil?
+    File.open('public/uploads/' + params['file_to_upload'][:filename], "w") do |f|
+      if f.write(params['file_to_upload'][:tempfile].read)
+        flash[:info] = "<div class=\"alert alert-success alert-dismissable\">Fichier envoy&eacute; avec succ&egrave;s.</div>"
+      else
+        flash[:warning] = "<div class=\"alert alert-error alert-dismissable\">Une erreur est survenue.</div>"
+      end
+    end
+  else
+    flash[:warning] = "<div class=\"alert alert-warning alert-dismissable\">Vous voulez uploader quelque chose d'inexistant ?!</div>"
+  end
 
-	redirect '/'
+  redirect '/'
 end
