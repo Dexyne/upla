dde34b0693b01f3ea23ec1a8436209400152cd19
Aurélien Léger <enyriel@gmail.com>
2014-07-03 15:42:51 +0200

    Order files by years and months and use method for flash messages.

--- upla.rb
+++ upla.rb
@@ -12,25 +12,35 @@ end
 # Handle POST-request (Receive and save the uploaded file)
 post '/upload' do
   unless params[:file_to_upload].nil?
-    upload_folder_path = 'public/uploads/'
+    upload_folder_path = './public/uploads/'
     year  = Time.now.year.to_s
     month = Time.now.strftime "%m"
 
-    Dir.mkdir("#{upload_folder_path}#{year}") unless File.exists?("#{upload_folder_path}#{year}")
-    Dir.mkdir("#{upload_folder_path}#{year}/#{month}") unless File.exists?("#{upload_folder_path}#{year}/#{month}")
+    FileUtils.mkdir_p "#{upload_folder_path}#{year}/#{month}" unless File.directory?("#{upload_folder_path}#{year}/#{month}")
 
     if File.directory?("#{upload_folder_path}#{year}/#{month}")
+      # TODO: add a try/catch
       File.open("#{upload_folder_path}#{year}/#{month}/" + params[:file_to_upload][:filename], "w") do |f|
         if f.write(params[:file_to_upload][:tempfile].read)
-          flash[:info] = "<div class=\"alert alert-success alert-dismissable\">Fichier envoy&eacute; avec succ&egrave;s.</div>"
+          flash_message(:info, "File uploaded.")
         else
-          flash[:warning] = "<div class=\"alert alert-error alert-dismissable\">Une erreur est survenue.</div>"
+          flash_message(:warning, "Error.")
         end
       end
     end
   else
-    flash[:warning] = "<div class=\"alert alert-warning alert-dismissable\">Vous voulez uploader quelque chose d'inexistant ?!</div>"
+    flash_message(:warning, "Do you want to upload nothing?!")
   end
 
   redirect '/'
 end
+
+private
+  # TODO: refactor
+  def flash_message(type = :info, message)
+    if type == :info
+      flash[type] = "<div class=\"alert alert-success alert-dismissable\">#{message}</div>"
+    else
+      flash[type] = "<div class=\"alert alert-error alert-dismissable\">#{message}</div>"
+    end
+  end
